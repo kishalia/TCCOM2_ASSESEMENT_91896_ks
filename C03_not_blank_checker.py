c6514fd0aed23d0bbc23bc1d13c6c8e94cc2cfb8
kishalia <158999987+kishalia@users.noreply.github.com>
2024-07-10 22:19:26 +1200

    Checks whether user responded to the question
    Checks whether response is blank or not.

---
+++ C03_not_blank_checker.py
@@ -0,0 +1,21 @@
+# function goes here
+
+# function checks if user responded with a blank.
+def not_blank(question):
+    while True:
+        response = input(question)
+
+        if response == "":
+            print("This cannot be blank.Please try again.")
+
+        else:
+            return response
+
+
+# main routine goes here
+while True:
+    name = not_blank("Please enter yes or no.").upper()
+    if name == 'QUIT':
+        break
+
+print("We are done!")
