a1b2d9a3573227b0f8a98abd65d10b82ef913dd8
kishalia <158999987+kishalia@users.noreply.github.com>
2024-07-17 13:15:50 +1200

    Adds coordinates then divides by 2 to output midpoint

---
+++ C06_midpoint_formula_VER1.py
@@ -0,0 +1,15 @@
+import math
+
+x1 = int(input("enter x1: "))
+y1 = int(input("enter y1: "))
+
+x2 = int(input("enter x2: "))
+y2 = int(input("enter y2: "))
+
+xm = (x1 + x2) / 2
+ym = (y1 + y2) / 2
+
+midpoint = xm,ym
+print(f"the 2 points are: {midpoint} ")
+
+
